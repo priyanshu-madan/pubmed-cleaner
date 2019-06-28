c74dc9babc915fc3857a524a5ada61a54328988c
priyanshu madan <pmadan.illinois@gmail.com>
2019-06-28 15:42:10 -0700

    commited

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
 
-S3_BUCKET = os.environ.get("S3_BUCKET")
-S3_KEY = os.environ.get("S3_KEY")
-S3_SECRET_ACCESS_KEY = os.environ.get("S3_SECRET_ACCESS_KEY")
+# S3_BUCKET = os.environ.get("S3_BUCKET")
+# S3_KEY = os.environ.get("S3_KEY")
+# S3_SECRET_ACCESS_KEY = os.environ.get("S3_SECRET_ACCESS_KEY")
 
+S3_BUCKET = "pubmed-db"
+S3_KEY = "AKIAIU5NCX7DBMNSR7GA"
+S3_SECRET_ACCESS_KEY = "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"
